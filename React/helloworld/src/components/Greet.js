51fe665a96ab018c5b8778c9e8868454fddb8b9b
Gaurav Dhabale <gauravdhabale77727@gmail.com>
2022-06-04 12:09:12 +0530

    functional and class component

--- React/helloworld/src/components/Greet.js
+++ React/helloworld/src/components/Greet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Greet() {
-  return <h1>Hello Gaurav</h1>;
-}
-
+// function Greet() {
+//   return <h1>Hello Gaurav</h1>;
+// }
+const Greet = () => <h1>Hello Gaurav</h1>;
 export default Greet;
